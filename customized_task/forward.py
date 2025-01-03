2a7f66401e8abb573975936ae2ebf91952b9f617
streamflowmaster <147464192+streamflowmaster@users.noreply.github.com>
2025-01-03 08:45:23 +0800

    Add files via upload

---
+++ customized_task/forward.py
@@ -0,0 +1,37 @@
+from collections import defaultdict
+from dataset import fintuning_dataloader
+import torch
+import torch.optim as optim
+from ComponentFiltering.utils.metrics import HQI,MSE,idf_acc
+import matplotlib.pyplot as plt
+
+def test_one_epoch(model,epoch,device,train_data):
+    num_sum = 0
+    hqi_sum,mse_sum = 0,0
+    # dict = return_dict().to(device)
+    for idx,(inputs,target) in enumerate(train_data):
+        inputs = inputs.float().to(device)
+        target = target.float().to(device)
+        outputs = model(inputs)
+        B,C,Lt = target.shape
+        B,C,Lo = outputs.shape
+        # print(Lt,Lo)
+        if Lt<Lo:
+            outputs = outputs[:,:,:Lt]
+        else:
+            target = target[:,:,:Lo]
+
+        plt.plot(inputs[1, 0, :].T.cpu().detach().numpy(), c='g')
+        plt.plot(target[1,0,:].T.cpu().detach().numpy(),c = 'r')
+        plt.plot(outputs[1, 0, :].T.cpu().detach().numpy(), c='b')
+        plt.show()
+
+def forward( model,device, batch_size=1):
+    train_data = fintuning_dataloader(batch_size=batch_size,device=device,snr=15)
+    metric = defaultdict(float)
+    model = model.to(device)
+    epoch = 0
+    with torch.no_grad():
+        test_one_epoch(model, epoch,
+                        device, train_data)
+        torch.cuda.empty_cache()
